d903c04a78e35ceea078b4ede1bf58be2068998e
tatiana <tatiana.76@kakaostyle.com>
2022-01-21 12:46:05 +0900

    command 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,4 @@
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 python3 /usr/local/bin/converter.py $GITHUB_WORKSPACE/${INPUT_LINT_XML_FILE}
-cat output_checkstyle.xml | reviewdog -f=checkstyle -name="Android Lint PR Review" -reporter="github-pr-review" -level="${INPUT_LEVEL}"
-${INPUT_REVIEWDOG_FLAGS} | reviewdog -f=checkstyle -name="Android Lint PR Check" -reporter="github-pr-check" -level="${INPUT_LEVEL}"
-${INPUT_REVIEWDOG_FLAGS}
+{ cat output_checkstyle.xml | reviewdog -f=checkstyle -name="Android Lint PR Review" -reporter="github-pr-review" -level="${INPUT_LEVEL}" ${INPUT_REVIEWDOG_FLAGS} } && { cat output_checkstyle.xml | reviewdog -f=checkstyle -name="Android Lint PR Check" -reporter="github-pr-check" -level="${INPUT_LEVEL}" ${INPUT_REVIEWDOG_FLAGS} }
