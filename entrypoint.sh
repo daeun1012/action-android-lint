afc1c4518535a7c8690834f886c5a864fc89aac5
tatiana <tatiana.76@kakaostyle.com>
2022-01-21 12:35:36 +0900

    command 변경

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 python3 /usr/local/bin/converter.py $GITHUB_WORKSPACE/${INPUT_LINT_XML_FILE}
-cat output_checkstyle.xml | reviewdog -f=checkstyle -name="Android Lint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" ${INPUT_REVIEWDOG_FLAGS}
+cat output_checkstyle.xml | reviewdog -f=checkstyle -name="Android Lint PR Review" -reporter="github-pr-review" -level="${INPUT_LEVEL}"
+${INPUT_REVIEWDOG_FLAGS} | reviewdog -f=checkstyle -name="Android Lint PR Check" -reporter="github-pr-check" -level="${INPUT_LEVEL}"
+${INPUT_REVIEWDOG_FLAGS}
